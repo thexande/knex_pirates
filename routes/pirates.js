34fbd623b7949d85a6eaf0c4ee448943145ed0bb
Alex <amurphy345@gmail.com>
2016-07-07 14:00:42 -0600

    adding pirates

--- routes/pirates.js
+++ routes/pirates.js
@@ -2,12 +2,39 @@ var express = require('express');
 var router = express.Router();
 var Pirates = require('../lib/Pirates')
 
-router.get('/pirates', function(req, res, next) {
-  // res.render('pirates/index');
-  Pirates.getAll().then((response) => {
-    res.json(response)
+// route to redirect to pirates table
+router.get('/', (req, res, next) => {
+  res.redirect('/pirates')
+})
+router.route('/pirates')
+  .get((req, res, next) => {
+    Pirates.getAll().then((response) => {
+      // render pirate table template
+      res.render('pirates/index', {pirates: response})
+      // res.json(response)
+    })
   })
-});
+  .post((req, res, next) => {
+    // create a new pirate
+  })
+router.get('/pirates/new', (req, res, next) => {
+  // display new pirate form
+  res.render('pirates/new')
+})
+router.post('/pirates/:id', (req, res, next) => {
+  // update an existing pirate
+})
+router.route('/pirates/:id/delete')
+  .get((req, res, next) => {
+    // delete a pirate
+  })
+  .post((req, res, next) => {
+    // delete a pirate
+  })
+router.get('/pirates/:id/edit',(req, res, next) => {
+  // edit a pirate, display pre filled form.
+})
 
+  // res.render('pirates/index');
 
 module.exports = router;
